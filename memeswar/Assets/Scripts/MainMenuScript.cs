f028deb42ce55a5da556073e55ba40b5449981aa
J. Santos <jamillo@gmail.com>
2016-05-14 12:31:02 -0300

    Base do menu principal #1

---
+++ memeswar/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+	public Button FindGame;
+
+	public Button CreateGame;
+
+	public Button Settings;
+
+	public Button Quit;
+
+	public Canvas QuitGameConfirmation;
+
+	void Start()
+	{
+		this.QuitGameConfirmation.enabled = false;
+	}
+
+	public void FindGameClick()
+	{
+
+	}
+
+	public void CreateGameClick()
+	{
+		//
+	}
+
+	public void SettingsClick()
+	{
+		//
+	}
+
+	public void QuitClick()
+	{
+		Debug.Log(this.QuitGameConfirmation);
+		this.QuitGameConfirmation.enabled = true;
+	}
+
+	public void QuitYesClick()
+	{
+		Application.Quit();
+	}
+
+	public void QuitNoClick()
+	{
+		this.QuitGameConfirmation.enabled = false;
+	}
+}
