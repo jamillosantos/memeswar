e4789f47a084fb4a125d444e53334e5a9799ba31
J. Santos <jamillo@gmail.com>
2016-05-23 18:56:33 -0300

    Implementação da base de armas e armas de fogo #24

---
+++ memeswar/Assets/Weapons/Scripts/Trigger.cs
@@ -0,0 +1,50 @@
+﻿
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class Trigger
+{
+	private bool _pulled;
+
+	private float _pulledAt;
+
+	public void Pull()
+	{
+		if (!this._pulled)
+		{
+			this._pulled = true;
+			this._pulledAt = Time.time;
+		}
+	}
+
+	public void Release()
+	{
+		this._pulled = false;
+	}
+
+	public float PulledAt
+	{
+		get
+		{
+			return this._pulledAt;
+		}
+	}
+
+	public float PulledElapsed
+	{
+		get
+		{
+			return Time.time - this._pulledAt;
+		}
+	}
+
+	public bool Pulled
+	{
+		get
+		{
+			return this._pulled;
+		}
+	}
+}
