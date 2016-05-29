62f1964c79e541ad6423209e64fafca81b469649
J. Santos <jamillo@gmail.com>
2016-05-28 23:18:20 -0300

    Implementação da troca do cursor por uma imagem (ícone da mira) #30

---
+++ memeswar/Assets/Scripts/MouseInteractions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseInteractions : MonoBehaviour
+{
+
+	public Camera Camera;
+
+	void Start ()
+	{
+		// Cursor.visible = false;
+	}
+
+	void FixedUpdate()
+	{
+		this.gameObject.transform.position = Input.mousePosition;
+	}
+}
