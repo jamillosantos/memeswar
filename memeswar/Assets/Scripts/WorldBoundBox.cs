11756e739acd8ee92015bec4a9dd4e50defd24d1
J. Santos <jamillo@gmail.com>
2016-06-17 15:51:07 -0300

    Implementação do script `WorldBoundBox` que representa o fim do mundo #63

---
+++ memeswar/Assets/Scripts/WorldBoundBox.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class WorldBoundBox : MonoBehaviour
+{
+	private const int TOP = 0;
+	private const int RIGHT = 1;
+	private const int BOTTOM = 2;
+	private const int LEFT = 3;
+
+	private const int COUNT = 4;
+
+	private BoxCollider[] _walls;
+
+	public float Width = 1f;
+
+	public float Height = 1f;
+
+	void Start()
+	{
+		this._walls = new BoxCollider[COUNT];
+		for (int i = 0; i < COUNT; i++)
+			this._walls[i] = this.gameObject.AddComponent<BoxCollider>();
+		this.applyTrans();
+	}
+
+	private void applyTrans()
+	{
+		if (this._walls != null)
+		{
+			this._walls[TOP].center = new Vector3(0f, -this.Height / 2f, 0f);
+			this._walls[TOP].size = new Vector3(this.Width, 1f, 1f);
+		
+			this._walls[RIGHT].center = new Vector3(this.Width / 2f, 0f, 0f);
+			this._walls[RIGHT].size = new Vector3(1f, this.Height, 1f);
+		
+			this._walls[BOTTOM].center = new Vector3(0f, this.Height / 2f, 0f);
+			this._walls[BOTTOM].size = new Vector3(this.Width, 1f, 1f);
+		
+			this._walls[LEFT].center = new Vector3(-this.Width / 2f, 0f, 0f);
+			this._walls[LEFT].size = new Vector3(1f, this.Height, 1f);
+		}
+	}
+
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.gray;
+		this.drawGizmos();
+	}
+
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.green;
+		this.drawGizmos();
+	}
+
+	private void drawGizmos()
+	{
+		Gizmos.DrawCube(this.transform.position + new Vector3(0f, -this.Height / 2f, 0f), new Vector3(this.Width, 1f, 1f));
+		Gizmos.DrawCube(this.transform.position + new Vector3(this.Width / 2f, 0f, 0f), new Vector3(1f, this.Height, 1f));
+		Gizmos.DrawCube(this.transform.position + new Vector3(0f, this.Height / 2f, 0f), new Vector3(this.Width, 1f, 1f));
+		Gizmos.DrawCube(this.transform.position + new Vector3(-this.Width / 2f, 0f, 0f), new Vector3(1f, this.Height, 1f));
+	}
+}
