049f93e6a9ae4ca4d8d5867e5e6435940e19fe77
J. Santos <jamillo@gmail.com>
2016-06-21 21:14:36 -0300

    Script da lua #61

---
+++ memeswar/Assets/Scripts/Moon.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moon : MonoBehaviour
+{
+
+	public float Radius = 1f;
+
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.green;
+		Gizmos.DrawSphere(this.transform.position, this.Radius);
+	}
+}
