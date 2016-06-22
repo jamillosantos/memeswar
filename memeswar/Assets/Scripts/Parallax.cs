8e514fb3e82a8b06e69b0af770d73240704ef621
J. Santos <jamillo@gmail.com>
2016-06-21 21:10:46 -0300

    Implementação do script do Parallax para aplicação no céu #61

---
+++ memeswar/Assets/Scripts/Parallax.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour
+{
+	public float Damping;
+
+	private Vector3 _initialPosition;
+
+	void Start()
+	{
+		this._initialPosition = this.transform.localPosition;
+	}
+
+	void Update ()
+	{
+		this.transform.localPosition = this._initialPosition + Camera.main.transform.position * this.Damping;
+	}
+}
